a521e35e9b017bd16002eeff08a5043c9f8b9714
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-22 23:54:31 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrescriptionIntegration::Application.config.secret_token = 'a03e24942141e857e43ba8dcb62706d0891b8eda97772ce1cd8fa4ca1c97bd705846723db39edce42f049dd3f3c912eb947f4e63ba2bcc92db79a4694f3b03bc'
