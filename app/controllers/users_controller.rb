5d09c027426070770f00c0cb9f9baa124e8c3fe2
eric <kureck@eric-pc.(none)>
2012-06-20 11:33:58 -0300

    mudancas em layout e traducoes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class UsersController < ApplicationController
   # GET /users
   # GET /users.json
@@ -35,5 +36,21 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
     @users = User.find(params[:id])
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'Usuário foi atualizado.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
