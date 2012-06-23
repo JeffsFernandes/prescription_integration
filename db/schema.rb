31c2a4b79909d2766dfbbcd52d11c54555dc1a1b
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-06-23 12:31:04 -0300

    adicionei quantidade no item e deixo vender para uso continuo tambem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120623130335) do
+ActiveRecord::Schema.define(:version => 20120623152928) do
 
   create_table "autorizados", :force => true do |t|
     t.string   "cpf"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20120623130335) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.string   "posologia"
+    t.string   "quantidade"
   end
 
   create_table "medicamentos", :force => true do |t|
